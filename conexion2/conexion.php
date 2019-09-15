268375c9f6ad013ee8cf1b049bfa16a338979844
NOELGN <noelgn@outlook.com>
2019-09-14 21:30:20 -0500

    Add files via upload

---
+++ conexion2/conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servidor="localhost"; //127.0.0.1
+$usuario="root";
+$contrasenia="";
+$bd="frutasnoes";
+
+$conectar= mysqli_connect($servidor,$usuario,$contrasenia,$bd) or die("Problemas al conectar con el servidor");//establecer la conexion
+
+/*if($conectar = true){
+    echo "conecto";
+}elseif($conectar = false){
+    echo "no conecto";
+}*/
+?>
